b63a3786156b0fc50bc0c0201288182c76af3b6b
Andrey Rogatin <AndreyR-Profit@yandex.ru>
2018-12-03 13:37:50 +0300

    add adminPanel

--- App/index.php
+++ App/index.php
@@ -1,14 +1,8 @@
 <?php
 
 use App\Models\Article;
-use App\Models\Product;
 
 require __DIR__ . '/autoload.php';
-
-$product = Product::findById(24);
-$product->delete();
-var_dump($product);
-
 
 $news = Article::getLastArticles(3);
 
