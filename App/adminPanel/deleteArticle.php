cadd94df9c29c4057c3bfa00fb2c87c7f70d6c3e
Andrey Rogatin <AndreyR-Profit@yandex.ru>
2018-12-03 14:22:24 +0300

    add deleteArticle in adminPanel

---
+++ App/adminPanel/deleteArticle.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Models\Article;
+
+require __DIR__ . '/../autoload.php';
+
+if (!empty($_GET['id'])) {
+    $article = Article::findById(abs((int)$_GET['id']));
+    $article->delete();
+}
+
+header('Location: /App/adminPanel/');
---
+++ App/templates/adminPanel.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Panel</title>
+</head>
+<body>
+    <h2>Админ Панель</h2>
+    <table border="1">
+        <tr>
+            <th>Заголовок новости</th>
+            <th>Удаление</th>
+            <th>Редактирование</th>
+        </tr>
+        <?php foreach($articles as $article) { ?>
+        <tr>
+            <td><?php echo $article->title; ?></td>
+            <td>
+                <a href="/App/adminPanel/deleteArticle.php?id=<?php echo $article->id; ?>">Удалить</a>
+            </td>
+            <td>
+                <a href="#">Редактировать</a>
+            </td>
+        </tr>
+        <?php } ?>
+    </table>
+    <h3>Добавить новость</h3>
+    <form action="#" method="post">Заголовок<br>
+        <input type="text" name="title" size="100" required>
+        <br>Текст<br>
+        <textarea name="body" cols="100" rows="10" required></textarea>
+        <br>Источник<br>
+        <input type="text" name="author" required><br>
+        <button type="submit">Отправить</button>
+    </form>
+    <?php
+    var_dump($articles);
+    ?>
+</body>
+</html>
