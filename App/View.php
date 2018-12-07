4671f9ec16aa46fcd544fad23ac37b9a1670c3ee
Andrey Rogatin <AndreyR-Profit@yandex.ru>
2018-12-07 14:26:07 +0300

    update View class

--- App/View.php
+++ App/View.php
@@ -7,6 +7,24 @@ class View
 {
     protected $data = [];
 
+    public function __set($name, $value)
+    {
+        $this->data[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        return $this->data[$name] ?? null;
+    }
+
+    public function __isset($name)
+    {
+        return isset($this->data[$name]);
+    }
+
+    /**
+     * @deprecated
+     */
     public function assign(string $name, $value)
     {
         $this->data[$name] = $value;
@@ -14,10 +32,19 @@ class View
 
     public function display(string $template)
     {
+        echo $this->render($template);
+    }
+
+    public function render(string $template)
+    {
         foreach ($this->data as $key => $value) {
             $$key = $value;
         }
 
+        ob_start();
         include $template;
+        $res = ob_get_contents();
+        ob_end_clean();
+        return $res;
     }
 }
--- index.php
+++ index.php
@@ -6,6 +6,5 @@ use App\View;
 require __DIR__ . '/autoload.php';
 
 $view = new View;
-$view->assign('news', Article::getLastArticles(3));
-
+$view->news = Article::getLastArticles(3);
 $view->display(__DIR__ . '/App/templates/index.php');
